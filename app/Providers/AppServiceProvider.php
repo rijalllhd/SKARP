73129f5826e58b198e71d63c4b02067e5c270c63
rijalllhd <ijal1445b@gmail.com>
2026-09-08 13:16:23 +0700

    fix: hapus kredensial dari commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Buat file firebase.json di Wasmer jika variabel env tersedia
+        if (env('FIREBASE_JSON_CONTENT')) {
+            $path = storage_path('app/firebase-credentials.json');
+            if (!file_exists($path)) {
+                if (!file_exists(storage_path('app'))) {
+                    mkdir(storage_path('app'), 0755, true);
+                }
+                file_put_contents($path, env('FIREBASE_JSON_CONTENT'));
+            }
+        }
     }
 }
